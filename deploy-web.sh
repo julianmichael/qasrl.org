41f042e65bf0aa181f1d4e6a4dead888420c0b76
julianmichael <julianjohnmichael@gmail.com>
2018-05-02 16:10:13 -0700

    Create bare-bones under-construction website with setup and deploy scripts

---
+++ deploy-web.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# usage: ./deploy.sh <profile-name>
+# where profile-name corresponds to a profile in your ~/.aws/credentials file
+# upload appropriate files to S3. TODO: change to using terraform with state in an S3 bucket
+
+aws s3 cp index.html s3://qasrl.org/index.html --profile $1
