6ba33353681eae179c592e0a2873ea79685709d7
julianmichael <julianjohnmichael@gmail.com>
2018-05-03 00:57:53 -0700

    Add error page

--- deploy-web.sh
+++ deploy-web.sh
@@ -5,3 +5,4 @@
 # upload appropriate files to S3. TODO: change to using terraform with state in an S3 bucket
 
 aws s3 cp index.html s3://qasrl.org/index.html --profile $1
+aws s3 cp error.html s3://qasrl.org/error.html --profile $1
